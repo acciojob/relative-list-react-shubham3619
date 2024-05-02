3a814a54a8ed744c803621d611c0eeaef08c4d55
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2024-05-02 03:28:11 +0000

    answer commited

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol key="relativeList">
+                  <li key="relativeListItem1">Ram</li>
+                  <li key="relativeListItem2">Shyam</li>
+                  <li key="relativeListItem3">Hari</li>
+               </ol>
     </div>
   )
 }
